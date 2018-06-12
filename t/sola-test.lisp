7a3872f37cd082bd3abc9a229138629950017ffa
Tomoki Aburatani <aburatanitomoki@gmail.com>
2018-06-12 15:46:34 +0900

    fix test about saekano

--- t/sola-test.lisp
+++ t/sola-test.lisp
@@ -15,6 +15,6 @@
 (subtest "Title regex get test"
   (let* ((saekano (sola::get-columns '(title) 
 		    (sola::get-anime-list :title ".*冴え.*" :update t))))
-    (ok (find "冴えない彼女の育てかた" (sola::get-sae) :test #'string= :key #'first))))
+    (ok (find "冴えない彼女の育てかた" saekano :test #'string= :key #'first))))
 
 (finalize)
